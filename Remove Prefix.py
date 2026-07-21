e443206d0ed31b7dff5f54fd114ab71b9677c4fc
Hayat Abdulfetah <hayatabdulfetah7@gmail.com>
2026-07-22 00:43:23 +0300

    Add solution to find last duplicate index
    Implement a solution to find the last index of a duplicate element in a list.

---
+++ Remove Prefix.py
@@ -0,0 +1,18 @@
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+
+    seen = set()
+    ans = 0
+
+    for i in range(n - 1, -1, -1):
+        if a[i] in seen:
+            ans = i + 1
+            break
+        seen.add(a[i])
+
+    print(ans)
+
+# problem link --> https://codeforces.com/problemset/problem/1714/B
