a4890e47c12cbeceebd3cb5dc8d2b21cbaa0c820
Hayat Abdulfetah <hayatabdulfetah7@gmail.com>
2026-08-08 23:05:30 +0300

    Update Assign Cookies.py

--- Assign Cookies.py
+++ Assign Cookies.py
@@ -4,11 +4,10 @@ class Solution:
         s.sort()
         i = 0
         j = 0
-        count = 0
+        
         while i < len(g) and j < len(s):
             if s[j] >= g[i]:
-                count += 1
                 i += 1
             j += 1
-            
-        return count
+
+        return i
