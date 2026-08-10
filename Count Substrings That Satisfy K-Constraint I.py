645ca1a72d07b262100c18efd5dbb549baa98174
Hayat Abdulfetah <hayatabdulfetah7@gmail.com>
2026-08-10 17:46:42 +0300

    Create Count Substrings That Satisfy K-Constraint I.py

---
+++ Count Substrings That Satisfy K-Constraint I.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countKConstraintSubstrings(self, s: str, k: int) -> int:
+        res = 0
+        n = len(s)
+        for i in range(n):
+            zeros = ones = 0
+            for j in range(i, n):
+                if s[j] == '0':
+                    zeros += 1
+                else:
+                    ones += 1
+                if zeros <= k or ones <= k:
+                    res += 1
+        return res
