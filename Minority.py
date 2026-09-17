737c5207d840dbe43829b3ba9f2726c5621bfcb0
Hayat Abdulfetah <hayatabdulfetah7@gmail.com>
2026-09-18 01:21:36 +0300

    Refactor Minority.py logic for counting bits

--- Minority.py
+++ Minority.py
@@ -1,16 +1,15 @@
 t = int(input())
-for _ in range(t):
-  s = input()
-  
-  ones = s.count('1')
-  zeros = s.count('0')
 
-  if ones < zeros:
-    print(ones)
-  elif ones > zeros:
-    print(zeros)
-  else:
-    print(0)
+for _ in range(t):
+    s = input()
+
+    ones = s.count('1')
+    zeros = s.count('0')
+
+    if ones != zeros:
+        print(min(ones, zeros))
+    else:
+        print(ones - 1)
 
 
 # Codeforces problem link --> https://codeforces.com/problemset/problem/1633/B
