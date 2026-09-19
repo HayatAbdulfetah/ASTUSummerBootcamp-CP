b3b02c80e6bf134db24ccd5cdbb32d51e58d0fe0
Hayat Abdulfetah <hayatabdulfetah7@gmail.com>
2026-09-20 00:23:07 +0300

    Add bit flipping logic for binary string comparison
    Implement logic to determine if two binary strings can be made equal by flipping bits.

--- Flip the Bits.py
+++ Flip the Bits.py
@@ -1,1 +1,37 @@
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = input()
+    b = input()
+    
+    # build balance info
+    good = [0]*n
+    zero = one = 0
+    
+    for i in range(n):
+        if a[i] == '0':
+            zero += 1
+        else:
+            one += 1
+        
+        if zero == one:
+            good[i] = 1
+    
+    flip = 0
+    
+    for i in range(n-1, -1, -1):
+        cur = a[i]
+        
+        # apply flip if needed
+        if flip:
+            cur = '1' if cur == '0' else '0'
+        
+        if cur != b[i]:
+            if good[i] == 0:
+                print("NO")
+                break
+            flip ^= 1
+    else:
+        print("YES")
+
 # Codeforces problem link --> https://codeforces.com/problemset/problem/1504/B
