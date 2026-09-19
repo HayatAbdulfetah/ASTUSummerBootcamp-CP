6374efd592b21ef521b77f64ecc91dd604eb4ad2
Hayat Abdulfetah <hayatabdulfetah7@gmail.com>
2026-09-19 22:30:52 +0300

    Implement solution for Codeforces problem 2148/D

--- Destruction of the Dandelion Fields.py
+++ Destruction of the Dandelion Fields.py
@@ -1,1 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input().strip())
+for i in range(t):
+    n = int(input().strip())
+    a = list(map(int, input().split()))
+    evens_sum = 0
+    odds = []
+    for v in a:
+        if v % 2 == 0:
+            evens_sum += v
+        else:
+            odds.append(v)
+    k = len(odds)
+    if k == 0:
+        print(0)
+        continue
+    odds.sort(reverse=True)
+    take = (k + 1) // 2
+    print(evens_sum + sum(odds[:take]))
+
 # Codeforces problem link --> https://codeforces.com/problemset/problem/2148/D
