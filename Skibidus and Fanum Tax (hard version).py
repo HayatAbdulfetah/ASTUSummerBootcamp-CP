f09b0d9314ef4150c4e27f0646ef826dbff7eb5a
Hayat Abdulfetah <hayatabdulfetah7@gmail.com>
2026-09-20 00:37:08 +0300

    Add solution for Skibidus and Fanum Tax problem
    Implement a solution for the Skibidus and Fanum Tax problem using binary search.

---
+++ Skibidus and Fanum Tax (hard version).py
@@ -0,0 +1,38 @@
+from bisect import bisect_left
+
+t = int(input())
+
+for _ in range(t):
+
+    n, m = map(int, input().split())
+
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+
+    b.sort()
+
+    prev = -10**18
+    possible = True
+
+    for x in a:
+
+        best = 10**18
+
+        if x >= prev:
+            best = x
+
+        need = prev + x
+
+        idx = bisect_left(b, need)
+
+        if idx < m:
+            new_value = b[idx] - x
+            best = min(best, new_value)
+
+        if best == 10**18:
+            possible = False
+            break
+
+        prev = best
+
+    print("YES" if possible else "NO")
